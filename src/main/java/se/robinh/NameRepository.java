69a60e7a14ba51f4d2e252127718937df2485443
Robin Homonai <159055771+RobinHomonai@users.noreply.github.com>
2024-03-13 12:56:51 +0100

    Solve part2

--- src/main/java/se/robinh/NameRepository.java
+++ src/main/java/se/robinh/NameRepository.java
@@ -10,9 +10,9 @@ public class NameRepository {
         return names.length;
     }
 
-    public static void setNames(String[] additionalNames) {
+    public static void setNames(String[] addNames) {
         // todo: Send in an array that set the private static array. This should replace all existing names.
-        names = Arrays.copyOf(additionalNames, additionalNames.length);
+        names = Arrays.copyOf(addNames, addNames.length);
     }
 
     public static void clear() {
@@ -23,5 +23,32 @@ public class NameRepository {
     public static String[] findAll() {
         // todo: Return all names in a new array.
         return Arrays.copyOf(names, names.length);
+    }
+
+    public static String find(final String fullName) {
+        // todo: Returns name if found and null if not found.
+        for (String name : names) {
+            if (name.equals(fullName)) {
+                return name;
+            }
+        }
+        return null;
+    }
+
+    public static boolean add(final String fullName) {
+        // todo: Should add a new name to the array. Returns true when name was added and false when the array contains the name.
+        for (String name : names) {
+            if (name.equals(fullName)) {
+                return false;
+            }
+        }
+
+        String[] addNames = new String[names.length + 1];
+        for (int i = 0; i < names.length; i++) {
+            addNames[i] = names[i];
+        }
+        addNames[addNames.length - 1] = fullName;
+        names = addNames;
+        return true;
     }
 }
